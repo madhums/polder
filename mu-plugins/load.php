33e66440cdeb3abeb36bfe561a34416a297793de
madhums <me@madhums.me>
2024-01-17 12:49:58 +0100

    add fluentform plugin

--- mu-plugins/load.php
+++ mu-plugins/load.php
@@ -2,6 +2,7 @@
 require(WPMU_PLUGIN_DIR . '/advanced-custom-fields-pro/acf.php');
 require(WPMU_PLUGIN_DIR . '/block-editor-bootstrap-blocks/block-editor-bootstrap-blocks.php');
 require(WPMU_PLUGIN_DIR . '/breadcrumb-navxt/breadcrumb-navxt.php');
+require(WPMU_PLUGIN_DIR . '/fluentform/fluentform.php');
 require(WPMU_PLUGIN_DIR . '/fluentformpro/fluentformpro.php');
 require(WPMU_PLUGIN_DIR . '/team-section/index.php');
 
