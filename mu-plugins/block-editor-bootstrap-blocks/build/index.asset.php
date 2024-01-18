7d687ecb194a021faa06440701d30953287ab75d
madhums <me@madhums.me>
2024-01-18 14:47:44 +0100

    add only the html exports of all pages

--- mu-plugins/block-editor-bootstrap-blocks/build/index.asset.php
+++
@@ -1,1 +0,0 @@
-<?php return array('dependencies' => array('react', 'wp-blockEditor', 'wp-blocks', 'wp-components', 'wp-compose', 'wp-data', 'wp-element', 'wp-hooks', 'wp-plugins', 'wp-polyfill', 'wp-primitives', 'wp-rich-text'), 'version' => 'ba92340ce534ab30d618271b51039c74');
