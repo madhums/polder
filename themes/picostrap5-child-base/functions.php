afb21a4ae5bd4b10b9991bab9a339fad5c8ddbef
madhums <me@madhums.me>
2024-02-14 11:32:24 +0100

    update

--- themes/picostrap5-child-base/functions.php
+++ themes/picostrap5-child-base/functions.php
@@ -243,3 +243,37 @@ function redirect_publication_detail_page() {
         }
     }
 }
+
+
+// display bootstrap classes in breadcrumbs
+function my_display_attributes_filter($attribs, $types, $id)
+{
+    $extra_attribs = array('class' => array('breadcrumb-item'));
+    //For the current item we need to add a little more info
+    if(is_array($types) && in_array('current-item', $types))
+    {
+        $extra_attribs['class'][] = 'active';
+        $extra_attribs['aria-current'] = array('page');
+    }
+    $atribs_array = array();
+    preg_match_all('/([a-zA-Z]+)=["\']([a-zA-Z0-9\-\_ ]*)["\']/i', $attribs, $matches);
+    if(isset($matches[1]))
+    {
+        foreach ($matches[1] as $key => $tag)
+        {
+            if(isset($matches[2][$key]))
+            {
+                $atribs_array[$tag] = explode(' ', $matches[2][$key]);
+            }
+        }
+    }
+    $merged_attribs = array_merge_recursive($atribs_array , $extra_attribs);
+    $output = '';
+    foreach($merged_attribs as $tag => $vals)
+    {
+        $output .= sprintf(' %1$s="%2$s"', $tag, implode(' ', $vals));
+    }
+    return $output;
+}
+
+add_filter('bcn_display_attributes', 'my_display_attributes_filter', 10, 3);
